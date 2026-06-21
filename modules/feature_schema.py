584006e68218b65041e00603ed32cd6f5e17e644
Anurag Bera <anuragbera2018@gmail.com>
2026-06-21 20:50:44 +0530

    Defined clinical feature schema

---
+++ modules/feature_schema.py
@@ -0,0 +1,29 @@
+ANEMIA_FEATURE_ORDER = [
+    "HGB",
+    "HCT",
+    "MCV",
+    "MCH",
+    "MCHC",
+    "RDW",
+    "RBC",
+    "FERRITTE",
+    "FOLATE",
+    "B12"
+]
+
+LIVER_FEATURE_ORDER = [
+    "Age",
+    "BMI",
+    "LFT - Total Bilirubin (mg/dL)",
+    "LFT - Direct Bilirubin (mg/dL)",
+    "LFT - SGOT (AST) (U/L)",
+    "LFT - SGPT (ALT) (U/L)",
+    "LFT - Alkaline Phosphatase (U/L)",
+    "LFT - Total Protein (g/dL)",
+    "LFT - Albumin (g/dL)",
+    "PT-INR",
+    "RFT - Urea (mg/dL)",
+    "RFT - Creatinine (mg/dL)",
+    "Ascites",
+    "Encephalopathy"
+]
