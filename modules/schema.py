e06992703f1d3a38fd91435ab6fda72a612c61a1
Anurag Bera <anuragbera2018@gmail.com>
2026-06-21 20:55:25 +0530

    Added data schema definitions

---
+++ modules/schema.py
@@ -0,0 +1,18 @@
+ALLOWED_LABS = [
+    # Anemia
+    "HGB", "FERRITTE", "MCV", "RBC", "RDW", "MCH", "MCHC", "HCT",
+
+    # Liver
+    "Age", "BMI",
+    "LFT - Total Bilirubin (mg/dL)",
+    "LFT - Direct Bilirubin (mg/dL)",
+    "LFT - SGOT (AST) (U/L)",
+    "LFT - SGPT (ALT) (U/L)",
+    "LFT - Alkaline Phosphatase (U/L)",
+    "LFT - Albumin (g/dL)",
+    "PT-INR",
+    "RFT - Urea (mg/dL)",
+    "RFT - Creatinine (mg/dL)",
+    "Ascites",
+    "Encephalopathy"
+]
