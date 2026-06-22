4ea99b3ab19e8de17c2141c952a059610eb5749b
Anurag Bera <anuragbera2018@gmail.com>
2026-06-22 20:09:42 +0530

    Added disease class mapping module

---
+++ modules/class_mapping.py
@@ -0,0 +1,13 @@
+ANEMIA_CLASS_MAP = {
+    0: "Healthy",
+    1: "HGB Anemia",
+    2: "Iron Deficiency Anemia",
+    3: "Folate Deficiency Anemia",
+    4: "Vitamin B12 Deficiency Anemia"
+}
+
+LIVER_CLASS_MAP = {
+    0: "Class A",
+    1: "Class B",
+    2: "Class C"
+}
